7da9eb282013e91e9fe575940174559a6385b0f8
AdrielZurita <148979500+AdrielZurita@users.noreply.github.com>
2023-11-08 11:48:19 -0500

    added animation

--- Assets/Scripts/Platform Movment.cs
+++ Assets/Scripts/Platform Movment.cs
@@ -30,7 +30,7 @@ public class PlatformMovement : MonoBehaviour
 
          if(horizontalInput < 0 ){
             transform.localScale = new Vector3(-1, 1, 1);
-         }else if(horizontalInput < 0){
+         }else if(horizontalInput > 0){
             transform.localScale = new Vector3(1, 1, 1);
          }
 
